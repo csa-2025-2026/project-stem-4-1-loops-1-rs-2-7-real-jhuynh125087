4e2c25800c5163bbc6ff99f366100422605adbe8
jhuynh125087 <jhuynh125@student.hbuhsd.edu>
2025-10-17 18:19:21 +0000

    first problem

--- Main.java
+++ Main.java
@@ -5,5 +5,16 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    // write a loop that prints the off numbers between 3 and 25
+
+    int count = 3;
+
+    while(count <= 25)
+    {
+      System.out.println(count);
+      count = count + 2;
+    }
+
+
   }
 }
